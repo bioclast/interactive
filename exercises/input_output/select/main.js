5f8cadcce83d69117e578b7b64b3bcd787fba384
bioclast <32444270+bioclast@users.noreply.github.com>
2022-03-15 02:00:24 -0400

    med

--- exercises/input_output/select/main.js
+++ exercises/input_output/select/main.js
@@ -1,16 +1,16 @@
 var day;
 var messages = [
-  "On Monday Ja Morant had 52 points.", 
-  "Jerami Grant went for 26 on Tuesday.", 
-  "On Wednesday Jrue Holiday had 11 rebounds.", 
-  "Moses Moody had 13 on Thursday.", 
-  "Happy Friday",
-  "On Saturday Lebron had 56 points.",
-  "On Sunday Kentucky beat South Carolina for the SEC title."
+  "Adderall is used to treat attention deficit hyperactivity disorder (ADHD) and narcolepsy (a sleep disorder).",
+  "Topical cocaine can be used as a local numbing agent to help with painful procedures in the mouth or nose.",
+  "3,4-Methyl enedioxy methamphetamine (MDMA), commonly known as ecstasy (℮) or molly, is a psychoactive drug primarily used for recreational purposes. The desired effects include altered sensations, increased energy, empathy, as well as pleasure. When taken by mouth, effects begin in 30 to 45 minutes and last 3 to 6 hours.",
+  "Ketamine is a medication primarily used for induction and maintenance of anesthesia.",
+  "Fluoxetine, sold under the brand names Prozac and Sarafem among others, is an antidepressant of the selective serotonin reuptake inhibitor (SSRI) class. It is used for the treatment of major depressive disorder, obsessive–compulsive disorder (OCD), bulimia nervosa, panic disorder, and premenstrual dysphoric disorder. It is also approved for treatment of major depressive disorder in adolescents and children 8 years of age and over. It has also been used to treat premature ejaculation. Fluoxetine is taken by mouth.",
+  "Methylphenidate, sold under the brand name Ritalin and Concerta among others, is a stimulant drug used to treat attention deficit-hyperactivity disorder (ADHD) and narcolepsy.",
+  "Diazepam is mainly used to treat anxiety, insomnia, panic attacks and symptoms of acute alcohol withdrawal."
 ];
 
-$("#days").change(function() {
-  day = $("#days").val();
+$("#meds").change(function() {
+  day = $("#meds").val();
   console.log(day);
   $(".message").html(messages[day]);
 })
